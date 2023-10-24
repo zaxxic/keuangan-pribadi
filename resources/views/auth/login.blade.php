4c9f75f49f1d6ac8d038f854e77b3523ac3278a6
zaxxic <renodzuhri@gmail.com>
2023-10-24 13:06:42 +0700

    benerin validasi login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,9 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
     <title>Login - Keungan</title>
-    <link rel="shortcut icon" href="assets/img/favicon.png" />
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
 </head>
@@ -21,20 +23,32 @@
                     <div class="card-body">
                         <h1 class="text-center mb-4">Login</h1>
                         @if (session('message'))
-                        <div class="alert alert-info">
-                            {{ session('message') }}
-                        </div>
+                            <div class="alert alert-info">
+                                {{ session('message') }}
+                            </div>
                         @endif
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
                             <div class="form-group">
-                                <label>Email</label>
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}"
-                                    autocomplete="email" autofocus required />
+                                <label class="form-control-label">Alamat email</label>
+                                <input id="email" type="text"
+                                    class="form-control @error('email') is-invalid @enderror" name="email"
+                                    value="{{ old('email') }}" autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label>Password</label>
-                                <input type="password" class="form-control" name="password" required />
+                                <div class="d-flex">
+                                    <input type="password" class="form-control" name="password" id="password"
+                                        required />
+                                    {{-- <span class="fas fa-eye toggle-password"
+                                        onclick="togglePasswordVisibility()"></span> --}}
+                                </div>
                             </div>
                             <div class="form-group">
                                 <div class="custom-control custom-checkbox">
@@ -55,9 +69,27 @@
         </div>
     </div>
 
+    <script>
+        function togglePasswordVisibility() {
+            var passwordField = document.getElementById("password");
+            var toggleIcon = document.querySelector(".toggle-password");
+
+            if (passwordField.type === "password") {
+                passwordField.type = "text";
+                toggleIcon.classList.remove("fa-eye");
+                toggleIcon.classList.add("fa-eye-slash");
+            } else {
+                passwordField.type = "password";
+                toggleIcon.classList.remove("fa-eye-slash");
+                toggleIcon.classList.add("fa-eye");
+            }
+        }
+    </script>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
